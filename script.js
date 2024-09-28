04759c0c982a6e5b5e9759ef4e190102e17a1b16
foreverexiled <foreverexiled23@gmail.com>
2024-09-29 01:06:05 +0500

    view restyling and form input validation and serialization

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
+function add() {
+    console.log($("#form-add").serializeArray());
+}
+
 $(function () {
     $('#btn-add').click(function () {
-        $('.view-main').css({
-            visibility: 'hidden',
-            'z-index': 0,
-        });
-        $('.view-add').css({
-            visibility: 'visible',
-            'z-index': 1,
-        });
+        setTimeout(() => {
+            $('.view-main').css({
+                visibility: 'hidden',
+                'z-index': 0,
+            });
+            $('.view-add').css({
+                visibility: 'visible',
+                'z-index': 1,
+            });
+        }, 300);
+    });
+    $('#form-add').submit(function (e) {
+        e.preventDefault();
+        add();
+        setTimeout(() => {
+            $('.view-add').css({
+                visibility: 'hidden',
+                'z-index': 0,
+            });
+            $('.view-main').css({
+                visibility: 'visible',
+                'z-index': 1,
+            });
+        }, 300);
     });
 });
